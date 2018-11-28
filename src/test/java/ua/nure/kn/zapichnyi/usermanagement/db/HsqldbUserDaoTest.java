07a178760a3cd7357e12380b1fec0a5ab6244883
Gringoire1482 <alex124181@gmail.com>
2018-11-28 23:11:28 +0200

    Step 10.Changes in HsqldbUserDaoTest for testing findAll method

--- src/test/java/ua/nure/kn/zapichnyi/usermanagement/db/HsqldbUserDaoTest.java
+++ src/test/java/ua/nure/kn/zapichnyi/usermanagement/db/HsqldbUserDaoTest.java
@@ -2,16 +2,21 @@ package ua.nure.kn.zapichnyi.usermanagement.db;
 
 import java.util.Date;
 
-import ua.nure.kn.zapichnyi.usermanagement.User;
-import junit.framework.TestCase;
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
 
-public class HsqldbUserDaoTest extends TestCase {
+import ua.nure.kn.zapichnyi.usermanagement.User;
+
+
+public class HsqldbUserDaoTest extends DatabaseTestCase {
  private HsqldbUserDao dao;
  private ConnectionFactory connectionFactory;
   
 	protected void  setUp() throws Exception {
 		super.setUp();
-		connectionFactory= new ConnectionFactoryImpl();
 		dao = new HsqldbUserDao(connectionFactory);
 	}
 
@@ -30,6 +35,19 @@ public class HsqldbUserDaoTest extends TestCase {
 		e.printStackTrace();
 		fail(e.toString()); 
 	}
+	}
+
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		connectionFactory= new ConnectionFactoryImpl();
+		return new DatabaseConnection(connectionFactory.createConnection());
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		IDataSet dataSet = new XmlDataSet(getClass().getClassLoader()
+				.getResourceAsStream("usersDataSet.xml"));
+		return dataSet;
 	}
 
 }
