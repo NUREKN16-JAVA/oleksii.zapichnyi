4acb55cf2032bfb57366f86c28bd344ed7103c71
Gringoire <alex124181@gmail.com>
2018-12-14 23:22:38 +0200

    Step 5 Created MainFrame class, BrowsePanel and main content panel

---
+++ src/main/java/ua/nure/kn/zapichnyi/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,11 @@
+package ua.nure.kn.zapichnyi.usermanagement.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+
+	public BrowsePanel(MainFrame mainFrame) {
+	
+	}
+
+}
--- src/main/java/ua/nure/kn/zapichnyi/usermanagement/gui/MainFrame.java
+++ src/main/java/ua/nure/kn/zapichnyi/usermanagement/gui/MainFrame.java
@@ -1,11 +1,52 @@
 package ua.nure.kn.zapichnyi.usermanagement.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
+	private static final int FRAME_HEIGHT = 600;
+	private static final int FRAME_WIDTH = 800;
+	private JPanel  contentPanel;
+	private JPanel browserPanel;
+
+	public MainFrame() {
+		super();
+		initialize();
+	}
+
+	private void initialize() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(FRAME_WIDTH,FRAME_HEIGHT);
+		this.setTitle("”правление пользовател€ми");
+		this.setContentPane(getContentPanel());
+		
+		
+	}
+
+	private Container getContentPanel() {
+		if(contentPanel==null) {
+			contentPanel= new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(),BorderLayout.CENTER);
+		}
+		return contentPanel;
+	}
+
+	private JPanel getBrowsePanel() {
+		if(browserPanel==null) {
+			browserPanel= new BrowsePanel(this);
+		}
+		return browserPanel;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	MainFrame frame = new MainFrame();
+	frame.setVisible(true);
+	
 
 	}
 
